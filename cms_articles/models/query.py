481fa4c7981fa809a476ba48f4d1196cc678dace
Jakub Dorňák <jakub.dornak@misli.cz>
2018-02-21 22:57:01 +0100

    inherit ArticleQuerySet from PageQuerySet

--- cms_articles/models/query.py
+++ cms_articles/models/query.py
@@ -1,15 +1,11 @@
-from cms.publisher.query import PublisherQuerySet
-from django.db.models import Q
-from django.utils import timezone
+from cms.models.query import PageQuerySet
 
 
-class ArticleQuerySet(PublisherQuerySet):
-    def published(self, language=None):
-        qs = self.filter(
-            Q(publication_date__lte=timezone.now()) | Q(publication_date__isnull=True),
-            Q(publication_end_date__gt=timezone.now()) | Q(publication_end_date__isnull=True),
-            title_set__published=True)
-        if language:
-            return qs.filter(title_set__language=language)
-        else:
-            return qs
+class ArticleQuerySet(PageQuerySet):
+
+    def on_site(self, site=None):
+        from cms.utils import get_current_site
+
+        if site is None:
+            site = get_current_site()
+        return self.filter(tree__node__site=site)
